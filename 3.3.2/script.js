e4862d4b6e01eca0157b336df1290e30b959b32a
vikcis3 <viktorie.kostliva@gmail.com>
2026-02-04 11:32:44 +0100

    nova slozka 332

---
+++ 3.3.2/script.js
@@ -0,0 +1,21 @@
+let userName = "Tomáš";
+console.log(userName)
+
+userName = "Honzík";
+console.log(userName)
+
+console.log(7+4)
+console.log("7"+"4")
+console.log("A"+"B")
+console.log("7"+4)
+
+let age = 31;
+console.log(age > 20);
+
+let text = "Text"
+console.log(typeof text)
+
+let a;
+console.log(a);
+let b = null;
+console.log(b)
