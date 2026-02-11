a9ff4411855a7073944598f8b917b71e547dd442
vikcis3 <viktorie.kostliva@gmail.com>
2026-02-11 11:45:49 +0100

    dcfhfh
    bbvfx

---
+++ cviceni 3.3.2/script.js
@@ -0,0 +1,9 @@
+userName = "Evžen";
+userName2 = "Kevin";
+
+let userNameAge = 67;
+let userName2Age = 69;
+
+let userNameAgeTotal = userNameAge + userName2Age;
+
+console.log("Součet věku lidí " + userName " a " + userName2 + " je " + userNameAgeTotal);
