c5363a8b74f07344f202cdd7b63abf499bed05a1
vikcis3 <viktorie.kostliva@gmail.com>
2026-03-04 11:39:24 +0100

    rzdhrrdt

--- cviceni 3.3.3/script.js
+++ cviceni 3.3.3/script.js
@@ -2,7 +2,7 @@ let totalPrice = 110;
 
 if (totalPrice >= 100) {
     let definitiveprice = totalPrice * 0.7
-    console.log("Celková cena je" + definitiveprice + " Kč.");
+    console.log("Celková cena je " + definitiveprice + " Kč.");
 } else {
     console.log("Utrať ještě" + (100 - totalPrice) + " Kč a získáš 30% slevu!")
 }
---
+++ cviceni 3.3.4/script.js
@@ -0,0 +1,9 @@
+let age = 20;
+
+if (age >= 3) 
+    console.log("Vstup zdarma!");
+if (age >= 18) {
+    console.log("Platíš plnou cenu.");
+}else {
+    console.log("Dostáváš slevu.")
+}
