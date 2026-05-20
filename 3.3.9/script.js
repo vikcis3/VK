8d830144a2d397c3b55ee6ecbb0417c16a1ee06a
vikcis3 <viktorie.kostliva@gmail.com>
2026-05-20 11:39:48 +0200

    rr

---
+++ 3.3.9/script.js
@@ -0,0 +1,23 @@
+console.log("It's a nice day");
+console.log('Moje jméno je "Evženie"')
+
+let username = "Tomáš"
+let message = `Moje jméno je ${username}`;
+console.log(message);
+
+let a = 5;
+let b = 10;
+
+console.log(`Součet je ${a+b}`);
+
+function pozdrav(name) {
+    return `Hello, ${name}!`;
+}
+
+console.log(`${pozdrav(`Honza`)}`)
+
+let popis = `Toto je delší text,
+který se rozprostírá
+přes několik řádků.`
+
+console.log(popis)
