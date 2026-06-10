f3472985dc5f74684e82e6c90c97726f6d4e28f7
vikcis3 <viktorie.kostliva@gmail.com>
2026-06-10 11:38:17 +0200

    lků

---
+++ 3.3.10/script.js
@@ -0,0 +1,12 @@
+let myString = "Hello world!";
+let myBadString = "Hello world!";
+ 
+console.log(myString.length);
+console.log(myString.toUpperCase());
+console.log(myString.toLowerCase());
+console.log(myString.charAt(1));
+console.log(myString.indexOf("l"));
+console.log(myString.replace("H", "P"));
+console.log(myString.replaceAll("Hello world", "Palworld"))
+console.log(myBadString.trim());
+ 
